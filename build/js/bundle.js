fe2a815f6bc23a69639f681bd40b2767b16e841a
Mahreen Qureshi <mahreenq@hotmail.com>
2018-02-02 23:03:05 -0500

    making slider reload on window width change

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -15,27 +15,53 @@ for (var i = 0; i < addCart.length; i++) {
 
 // changes number of slides depending on width of screen on slider
 
-if ($(window).width() < 600) {
-  $('.bxSlider').bxSlider({
-    maxSlides: 1,
-    minSlides: 1,
-    slideWidth: 400,
-    auto: true
-  });
-} else if ($(window).width() < 1000) {
-  $('.bxSlider').bxSlider({
-    maxSlides: 2,
-    minSlides: 2,
-    slideWidth: 400,
-    auto: true
-  });
-} else {
-  $('.bxSlider').bxSlider({
-    maxSlides: 4,
-    minSlides: 4,
-    slideWidth: 400,
-    auto: true
-  });
+// if ($(window).width() < 600) {
+//     $('.bxSlider').bxSlider({
+//     maxSlides:1,
+//     minSlides:1,
+//     slideWidth: 400,
+//     auto: true,
+//   });
+// } else if ($(window).width() < 1000){
+//   $('.bxSlider').bxSlider({
+//     maxSlides:2,
+//     minSlides:2,
+//     slideWidth: 400,
+//     auto: true,
+//   });
+// } else {
+//   $('.bxSlider').bxSlider({
+//     maxSlides:4,
+//     minSlides:4,
+//     slideWidth: 400,
+//     auto: true,
+//   });
+// }
+
+
+var slider,
+    oBxSettings = {
+  maxSlides: 1,
+  minSlides: 1,
+  slideWidth: 400,
+  auto: true
+};
+
+function init() {
+  oBxSettings.maxSlides = window.outerWidth < 600 ? 1 : 4;
 }
+
+$(document).ready(function () {
+  init();
+  slider = $('.bxSlider').bxSlider(oBxSettings);
+});
+
+$(window).resize(function () {
+  if (window.outerWidth < 600 && window.prevWidth >= 600 || window.outerWidth >= 600 && window.prevWidth < 600) {
+    init();
+    slider.reloadSlider(oBxSettings);
+  }
+  window.prevWidth = window.outerWidth;
+});
 
 },{}]},{},[1]);
--- src/js/index.js
+++ src/js/index.js
@@ -14,25 +14,53 @@ var addCart = document.getElementsByClassName("addtocart");
 
 // changes number of slides depending on width of screen on slider
 
-if ($(window).width() < 600) {
-    $('.bxSlider').bxSlider({
+// if ($(window).width() < 600) {
+//     $('.bxSlider').bxSlider({
+//     maxSlides:1,
+//     minSlides:1,
+//     slideWidth: 400,
+//     auto: true,
+//   });
+// } else if ($(window).width() < 1000){
+//   $('.bxSlider').bxSlider({
+//     maxSlides:2,
+//     minSlides:2,
+//     slideWidth: 400,
+//     auto: true,
+//   });
+// } else {
+//   $('.bxSlider').bxSlider({
+//     maxSlides:4,
+//     minSlides:4,
+//     slideWidth: 400,
+//     auto: true,
+//   });
+// }
+
+
+
+ var slider,
+  oBxSettings = { 
     maxSlides:1,
     minSlides:1,
     slideWidth: 400,
     auto: true,
-  });
-} else if ($(window).width() < 1000){
-  $('.bxSlider').bxSlider({
-    maxSlides:2,
-    minSlides:2,
-    slideWidth: 400,
-    auto: true,
-  });
-} else {
-  $('.bxSlider').bxSlider({
-    maxSlides:4,
-    minSlides:4,
-    slideWidth: 400,
-    auto: true,
-  });
+  };
+
+function init() {
+  oBxSettings.maxSlides = window.outerWidth < 600 ? 1 : 4;
 }
+
+$(document).ready(function() {
+  init();
+  slider = $('.bxSlider').bxSlider(oBxSettings);
+});
+
+$(window).resize(function() {
+  if ((window.outerWidth<600 && window.prevWidth>=600)
+    || (window.outerWidth>=600 && window.prevWidth<600)) {
+    init();
+    slider.reloadSlider(oBxSettings);
+  }
+  window.prevWidth = window.outerWidth;
+});
